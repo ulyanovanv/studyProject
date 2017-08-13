399c10b7f8c213724d33e9da5a4642d3d865e0e2
ulyanovanv <av.ulyanova@gmail.com>
2017-08-13 22:17:46 +0200

    New commit test phpStorm

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         <link href="/studyProject/public/css/main.css" rel="stylesheet" />
 	</head>
 	<body>
-		<h1> time is 22:13</h1>
+		<h1> time is 22:13 g</h1>
 		<div> kakay tupost </div>
+    <div>Privet</div>
 	</body>
 </html>
