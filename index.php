98bdbe5987615ddb00b06a41b54c41cc0e79989d
ulyanovanv <av.ulyanova@gmail.com>
2017-08-13 22:33:49 +0200

    new libraries connected

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <html>
 	<head>
         <link href="/studyProject/public/css/main.css" rel="stylesheet" />
+        <script src="/studyProject/node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="/studyProject/public/js/main.js"></script>
 	</head>
 	<body>
 		<h1> time is 22:13 g</h1>
 		<div> kakay tupost </div>
-    <div>Privet</div>
+        <div>Privet</div>
 	</body>
 </html>
