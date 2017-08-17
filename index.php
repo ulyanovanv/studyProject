5c725b244fdff34311dfddcc4c5fc12cd1db01b3
ulyanovanv <av.ulyanova@gmail.com>
2017-08-17 22:53:20 +0200

    silex added

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
 	<head>
         <link href="/studyProject/public/css/main.css" rel="stylesheet" />
-        <script src="/studyProject/node_modules/jquery/dist/jquery.min.js"></script>
-        <script src="/studyProject/public/vendor/jquery-ui.min.js"></script>
-        <script src="/studyProject/public/js/main.js"></script>
 	</head>
 	<body>
 		<h1> time is 22:13 g</h1>
 		<div> kakay tupost </div>
         <div>Privet</div>
+        <script src="/studyProject/node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="/studyProject/public/vendor/jquery-ui.min.js"></script>
+        <script src="/studyProject/public/js/main.js"></script>
 	</body>
 </html>
