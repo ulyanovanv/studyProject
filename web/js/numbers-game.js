2ff9516bb71bfdbb27f0e5108da2d2ab3ea79c05
Anastasiia <av.ulyanova@gmail.com>
2017-09-26 22:51:32 +0200

    numbers game improvements

--- web/js/numbers-game.js
+++ web/js/numbers-game.js
@@ -7,77 +7,71 @@ $(document).ready(function(){
        $("#first").hide();
        $("#second").show();
    })
-})
+});
 
 var counter = 1;
 var seconds = 10;
-var colors = ["#61b861","#ec8b68","#d7666b","#403c3b","#0077cc","#003580","#78052d"];
+const colors = ["#61b861","#ec8b68","#d7666b","#403c3b","#0077cc","#003580","#78052d"];
 
 function createNumbers() {
-   for (var i=1;i<10;i++) {
-       var num = Math.ceil(Math.random()*9);
-       var currentCell = $("#c"+num);
-       var col = Math.ceil(Math.random()*7);
+   for (var i = 1; i < 10; i++) {
+       var num = Math.ceil(Math.random() * 9);
+       var currentCell = $("#c" + num);
+       var col = Math.ceil(Math.random() * 7);
        currentCell.css({color:colors[col]});
-//       console.log(currentCell);
-//       console.log(i);
        if (currentCell.text()) {
            i = i-1;
        } else {
-       currentCell.text(i);
+           currentCell.text(i);
        }
    }
 }
 
 
 $(function(){
-    $(".cell").on("click",function(){
-//        console.log($(".cell"));
-        if ($(this).text()==counter) {
+    $(".cell").on("click",function() {
+        if ($(this).text() == counter) {
             $(this).removeClass("initial-color").addClass("red");
-//            console.log(counter);
             counter++;
         }
         var cells = document.getElementsByClassName("cell");
-//        console.log(cells);
-        for (var j = 0; j < 9; j++){
+        for (var j = 0; j < 9; j++) {
             var cc = $(cells[j]);
-//            console.log(cc);
-            if (cc.hasClass("red")===false) {
+            if (cc.hasClass("red") === false) {
                 break;
             }
-            if (cc.hasClass("red") && j==8) {
+            if (cc.hasClass("red") && j === 8) {
                 $("#time-left").html("Вы выиграли");
             }
         }
      })
-})
+});
 
 
 
-function timer(){
+function timer() {
     var intervalId = setInterval(function(){
         seconds--;
-        if (seconds<0){
+        if (seconds < 0) {
             $("#time-left").html("Вы проиграли");
             counter=10;
             clearInterval(intervalId);
             return;
-        };
+        }
         if (counter==10) {
             clearInterval(intervalId);
             return;
         }
         $("#seconds").text(seconds);
-        console.log(seconds);
-    },1000)
+    }, 1000)
 }
 
-function gameReset(){
-    $(".cell").text("");
+function gameReset() {
+    const cell = $(".cell");
+    cell.text("");
     createNumbers();
-    counter=1;
-    $(".cell").removeClass("red").addClass("initial-color");
+    counter = 1;
+    cell.removeClass("red").addClass("initial-color");
     $("#time-left").html("Времени осталось:" + "<span id='seconds'>10</span>");
     timer();
     seconds = 10;
@@ -88,4 +82,4 @@ function gameReset(){
 $(function(){
     $("#new-game").on("click", gameReset);
     $("#beginning-button").on("click", gameReset);
-})
+});
