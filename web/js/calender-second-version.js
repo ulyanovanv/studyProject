ef3e0d2484fa69f9724d4fbfbe1d92610d761f5b
Anastasiia <av.ulyanova@gmail.com>
2017-10-04 16:33:08 +0200

    calender finished

--- web/js/calender-second-version.js
+++ web/js/calender-second-version.js
@@ -4,12 +4,20 @@
 const months = ["Januar","Februar","March","April","May","June","July","August","September","October","November","December"];
 var parentDiv = $("#third-line");
 
-var currentMonth = 2;
-var currentYear = 2017;
 
-var currentDate = new Date(currentYear,currentMonth);
+var today = new Date();
+var currentDate = new Date(today.getFullYear(), today.getMonth(), 1);
 var currentMonth = currentDate.getMonth();
 var currentWeekDay = currentDate.getDay().toString();
+var currentYear = currentDate.getFullYear();
+
+
+$(document).ready(function(){
+    createEmptyCells(currentWeekDay, parentDiv);
+    createCalenderDates(nextDate, parentDiv);
+    $("#year").text(currentYear);
+    $("#month").text(months[currentMonth]);
+});
 
 var nextDate = new Date(currentYear,currentMonth + 1);
 var nextMonthh = nextDate.getMonth();
@@ -17,134 +25,155 @@ var nextMonthh = nextDate.getMonth();
 var previousDate = new Date(currentYear,currentMonth - 1);
 var previousMonth = previousDate.getMonth(previousDate);
 
-$(document).ready(function(){
-    findEmptyCells();
-    addEmptyCells();
-    createCalenderDates();
-})
-
-
 
 function createNextCalenderMonth(){
 
     parentDiv.html("");
-
     previousDate =  currentDate;
     previousMonth = currentMonth;
 
-
-    currentDate = nextDate;
+    currentDate = new Date(nextDate);
     currentMonth = nextMonthh;
-
+    currentYear = currentDate.getFullYear();
+    currentWeekDay = currentDate.getDay().toString();
 
     $("#year").text(currentYear);
     $("#month").text(months[currentMonth]); //writing the year and month -------
-    //    if (months[newMonth] == 11) {
-//        year = year +1;
-//        $("#year").text(year);
-//    } else {
-//        $("#year").text(year);
-//    }
 
-    addEmptyCells(); //we add cells before current month
+    createEmptyCells(currentWeekDay,parentDiv);
 
     nextDate = nextDate.setMonth(nextDate.getMonth()+1); //milliseconds
     nextDate = new Date(nextDate);
     nextMonthh = nextDate.getMonth();
-    console.log(nextDate);
-    console.log(nextMonthh);
-    console.log(currentDate);
 
-    createCalenderDates();
+    createCalenderDates(nextDate,parentDiv)
 }
 
-//function createPreviousCalenderMonth(){
-//    parentDiv.html("");
-//
-//    previousDate =  currentDate;
-//    previousMonth = currentMonth;
-//    console.log(previousDate);
-//    console.log(previousMonth);
-//
-//
-//    currentDate = nextDate;
-//    currentMonth = nextMonthh;
-//    console.log(currentDate);
-//    console.log(currentMonth);
-//
-//
-//
-//    $("#year").text(currentYear);
-//    $("#month").text(months[currentMonth]);
-//
-//
-//    addEmptyCells(); //we add cells before current month
-//
-//
-//    nextDate = nextDate.setMonth(nextDate.getMonth()+1); //milliseconds
-//    nextDate = new Date(nextDate);
-//    nextMonthh = nextDate.getMonth();
-//    console.log(nextDate);
-//    console.log(nextMonthh);
-//    console.log(currentDate);
-//
-//    var findLastDayOfCurrentMonth = nextDate.setDate(nextDate.getDate()-1);
-//    var lastDayOfCurrentMonth = new Date(findLastDayOfCurrentMonth);
-//    var lastDay =lastDayOfCurrentMonth.getDate();
-//
-//    for (var number = 1; number <= lastDay; number ++) {
-//        parentDiv.append("<div class='day'>"+number+"</div>");
-//    }
-//}
+function createPreviousCalenderMonth(){
+    parentDiv.html("");
+    nextDate =  currentDate;
+    nextMonthh = currentMonth;
+
+    currentDate = new Date(previousDate);
+    currentMonth = previousMonth;
+
+    currentYear = currentDate.getFullYear();
+    currentWeekDay = currentDate.getDay().toString();
+
+    $("#year").text(currentYear);
+    $("#month").text(months[currentMonth]);
+
+    createEmptyCells(currentWeekDay,parentDiv);
+
+    previousDate = previousDate.setMonth(previousDate.getMonth()-1); //milliseconds
+    previousDate = new Date(previousDate);
+    previousMonth = previousDate.getMonth();
+
+    createCalenderDates(nextDate,parentDiv)
+}
 
 
-function findEmptyCells(){
-    for(var i = currentWeekDay-1; i>0; i--){
-        parentDiv.append("<div class='day blue'>0</div>");
+function findEmptyCells(currentWeekDay, div) {
+    var yesterday = new Date(currentDate);
+    var findLastDayOfCurrentMonth = yesterday.setDate(currentDate.getDate()-1);
+    var lastDayOfCurrentMonth = new Date(findLastDayOfCurrentMonth);
+    var lastDay = lastDayOfCurrentMonth.getDate();
+    var lastWeekDay = lastDayOfCurrentMonth.getDay();
+
+    var array = [];
+    for(var ll = lastDay - lastWeekDay + 1; ll<=lastDay; ll++){
+        array.push(ll);
+    }
+    var xCounter = 0;
+    for(var i = currentWeekDay - 1; i > 0; i--){
+        div.append("<div class='day blue'>" + array[xCounter] +"</div>");
+        xCounter++;
     }
 }
-function addEmptyCells(){
+
+function createEmptyCells(currentWeekDay, parentDiv) {
     switch (currentWeekDay) {
         case "2":
-            findEmptyCells();
-            break;
         case "3":
-            findEmptyCells();
-            break;
         case "4":
-            findEmptyCells();
-            break;
         case "5":
-            findEmptyCells();
-            break;
         case "6":
-            findEmptyCells();
+            findEmptyCells(currentWeekDay, parentDiv);
             break;
         case "0":
+            var yesterday = new Date(currentDate);
+            var findLastDayOfCurrentMonth = yesterday.setDate(currentDate.getDate()-1);
+            var lastDayOfCurrentMonth = new Date(findLastDayOfCurrentMonth);
+            var lastDay = lastDayOfCurrentMonth.getDate();
+            var lastWeekDay = lastDayOfCurrentMonth.getDay();
+
+            var array = [];
+            for(var ll = lastDay - lastWeekDay + 1; ll<=lastDay; ll++){
+                array.push(ll);
+            }
+            var xCounter = 0;
             for(var i = 6; i>0; i--){
-                parentDiv.append("<div class='day blue'>0</div>");
+                parentDiv.append("<div class='day blue'>" + array[xCounter] +"</div>");
+                xCounter++;
             }
             break;
     }
-}
-function createCalenderDates(){
-    var findLastDayOfCurrentMonth = nextDate.setDate(nextDate.getDate()-1);
+};
+
+function createCalenderDates(nextDate, parentDiv){
+    var yesterday = new Date(nextDate);
+    console.log(yesterday);
+//    var currentWeekDay = yesterday.getDay();
+    var findLastDayOfCurrentMonth = yesterday.setDate(nextDate.getDate()-1); //?
     var lastDayOfCurrentMonth = new Date(findLastDayOfCurrentMonth);
-    var lastDay =lastDayOfCurrentMonth.getDate();
+    var lastDay = lastDayOfCurrentMonth.getDate();
+    var currWeekDay = lastDayOfCurrentMonth.getDay().toString();
+    console.log(currWeekDay);
 
     for (var number = 1; number <= lastDay; number ++) {
-        parentDiv.append("<div class='day'>"+number+"</div>");
+        var date = new Date(yesterday.getFullYear(), yesterday.getMonth(), number);
+        var newDay = $("<div class='day'>"+number+"</div>");
+        parentDiv.append(newDay);
+
+        if (today.toDateString() == date.toDateString()){
+            newDay.addClass("current-date");
+        }
+    }
+    createNewDaysOfNextMonths(currWeekDay);
+
+}
+function createNewDaysOfNextMonths(currWeekDay){
+    var addDateCounter;
+    switch(currWeekDay) {
+        case "1":
+            addDateCounter = 6;
+            break;
+        case "2":
+            addDateCounter = 5;
+            break;
+        case "3":
+            addDateCounter = 4;
+            break;
+        case "4":
+            addDateCounter = 3;
+            break;
+        case "5":
+            addDateCounter = 2;
+            break;
+        case "6":
+            addDateCounter = 1;
+            break;
+    }
+    for (var i = 1; i <= addDateCounter;i++){
+        var nDay = $("<div class='day blue'>"+i+"</div>");
+        parentDiv.append(nDay);
     }
 }
-
-
 
 
 $(function(){
     $("#to-right").on("click", createNextCalenderMonth);
 })
-//$(function(){
-//    console.log("he");
-//    $("#to-left").on("click",createPreviousCalenderMonth);
-//})
-
+$(function(){
+    $("#to-left").on("click",createPreviousCalenderMonth);
+})
