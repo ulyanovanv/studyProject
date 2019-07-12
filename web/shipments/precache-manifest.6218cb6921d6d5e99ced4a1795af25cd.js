5229223a3d63da05c994c3f52c544964744625f4
Anastasiia <av.ulyanova@gmail.com>
2019-07-12 12:30:17 +0200

    Add Shipments project to CV

---
+++ web/shipments/precache-manifest.6218cb6921d6d5e99ced4a1795af25cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552dfbb9f82bb5c8d4ee115cd1be6e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7917bb25b472a52d637",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4b9b8fc3bc28c37d5c75",
+    "url": "/static/css/main.0eadc13e.chunk.css"
+  },
+  {
+    "revision": "b7917bb25b472a52d637",
+    "url": "/static/js/2.b0e21c00.chunk.js"
+  },
+  {
+    "revision": "4b9b8fc3bc28c37d5c75",
+    "url": "/static/js/main.f2c2eb07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
